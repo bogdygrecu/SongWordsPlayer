228d11c4609943a86f10e89be149ff128af7e518
bogdygrecu <bogdy_grecu@yahoo.com>
2018-02-13 02:18:34 +0200

    Implemented the mechanism to search for music files in the music location folders

--- SongWordsPlayer/Frames/MyMusicFrame.xaml.cs
+++ SongWordsPlayer/Frames/MyMusicFrame.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,49 @@ namespace SongWordsPlayer.Frames
     /// </summary>
     public sealed partial class MyMusicFrame : Page
     {
+        private ObservableCollection<StorageFile> myMusic;  //consider moving this to main page as public (avoid losing everything when reload the page)
+        private ObservableCollection<StorageFolder> myLocations;
+
         public MyMusicFrame()
         {
             this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool IsTargetedFileType(StorageFile file)
+        {
+            if(file.FileType == ".mp3") //TODO: add more types later (document what types can be played)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private async Task SearchForMusicFilesInLocationAsync(StorageFolder parent)
+        {
+            foreach (StorageFile item in await parent.GetFilesAsync())
+            {
+                if(IsTargetedFileType(item))
+                {
+                    myMusic.Add(item);
+                }
+            }
+            foreach (StorageFolder item in await parent.GetFoldersAsync())
+            {
+                await SearchForMusicFilesInLocationAsync(item);
+            }
+        }
+
+        private async Task UpdateMusicCollectionAsync()
+        {
+            foreach(var location in myLocations)
+            {
+                await SearchForMusicFilesInLocationAsync(location);
+            }
         }
     }
 }
